04cf142cc537883e7bdc81acf4dd9b2e18312e23
derekearl <ear17004@byui.edu>
2023-05-27 14:07:30 -0600

    recommit

--- server.js
+++ server.js
@@ -6,14 +6,30 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const baseController = require("./controllers/baseController")
 
+
+/* ***********************
+ * View Engine and Templates
+ * ***********************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+// Index Route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+app.get("/", baseController.buildHome)
 
+// Inventory routes
+app.use("/inv", require("./routes/inventoryRoute"))
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
