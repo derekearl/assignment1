ee9681edfd4d8d1a435ab3893cee472561e0f6b0
derekearl <ear17004@byui.edu>
2023-07-07 10:34:09 -0600

    trial4.0

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -14,14 +14,20 @@ router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.b
 //Route to logout
 router.get("/logout", utilities.handleErrors(accountController.accountLogout))
 //Route to update account
-router.get("/update-account/:accId", utilities.handleErrors(accountController.buildEditAccount))
+router.get("/update-account/:accId", utilities.checkLogin, utilities.handleErrors(accountController.buildEditAccount))
+//Route to messages center
+router.get('/messages/:accId', utilities.checkLogin, utilities.handleErrors(accountController.buildMessages))
+//Route to new message
+router.get('/send-message/:accId', utilities.checkLogin, utilities.handleErrors(accountController.buildNewMessage))
 //Route to send registration info
-router.post("/register", regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
+router.post('/register', regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 // Process the login attempt
 router.post("/login", regValidate.loginRules(), regValidate.checkLogData, utilities.handleErrors(accountController.accountLogin))
 //Route to send update info
-router.post("/updateAccount", regValidate.updateAccountRules(), regValidate.checkUpdateData, utilities.handleErrors(accountController.accountUpdate))
+router.post("/updateAccount", utilities.checkLogin, regValidate.updateAccountRules(), regValidate.checkUpdateData, utilities.handleErrors(accountController.accountUpdate))
 //Route to send update info
-router.post("/updatePassword", regValidate.updateAccountPasswordRules(), regValidate.checkUpdatePasswordData, utilities.handleErrors(accountController.accountPasswordUpdate))
+router.post("/updatePassword", utilities.checkLogin, regValidate.updateAccountPasswordRules(), regValidate.checkUpdatePasswordData, utilities.handleErrors(accountController.accountPasswordUpdate))
+//Route to send a new message
+//router.post('/send_message', utilities.checkLogin, regValidate.newMessageRules(), regValidate.checkNewMessageData, utilities.handleErrors(accountController.getNewMessage) )
 
 module.exports = router;
