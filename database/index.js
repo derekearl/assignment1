ec7fccbdedbc9c13a7b36f39be4f209807789eef
derekearl <ear17004@byui.edu>
2023-07-08 23:06:26 -0600

    Experiment

--- database/index.js
+++ database/index.js
@@ -23,7 +23,8 @@ module.exports = {
       const res = await pool.query(text, params)
       console.log("executed query", { text })
       return res
-    } catch (error) {
+    } catch
+     (error) {
       console.error("error in query", { text })
       throw error
     }
